f356ece5efc17075eef590c6d02c044278674980
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-04 20:09:49 -0300

    feat: implementando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import styles from './styles.module.scss'
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 function App() {
   return (
     <div>
-     Todo
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
+      </main>
+     <Footer />
     </div>
   );
 }
