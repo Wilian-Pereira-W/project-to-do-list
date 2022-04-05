94185b783f12fa2b4b613db893f08b2c65596af7
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-04 22:55:57 -0300

    feat: implementando o componente modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from './styles.module.scss';
+
+interface Props {
+  children: React.ReactNode
+}
+
+const Modal = ({ children }: Props) => {
+
+  const closeModal = (e: React.MouseEvent): void => {
+    const modal = document.querySelector("#modal");
+    modal!.classList.add('hide');
+  };
+
+  return(
+    <div id="modal" className="hide">
+      <div className={styles.fade} onClick={closeModal}>
+
+      </div>
+      <div className={styles.modal}>
+        <h2>Modal</h2>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
